54e704566f0b9562f7f63f7d87710cb3f06bd227
Ray Heasman <ray@decentespresso.com>
2021-11-19 15:04:50 -0800

    Initial checkin

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
+import Dashboard from './dashboard/Dashboard'
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <CssBaseline />
+    <Dashboard />
+  </ThemeProvider>,
+  document.querySelector('#root'),
+);
